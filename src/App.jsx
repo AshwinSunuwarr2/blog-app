05acbade67e625d9415af64f594e2f0f9cee8609
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-02-11 09:11:09 +0545

    auth file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <div className="bg-red-500">this is appwrite app.</div>
---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //log in that user
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
